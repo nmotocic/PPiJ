0095d4cb611c113f60c0a3e02aed8cc71f7d1a4c
Dorijan_Cirkveni <dorijan.cirkveni@engineer.com>
2019-04-02 17:33:09 +0200

    valjda sad

---
+++ GrupaTNT/Assets/Scripts/DispenserScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispenserScript : MonoBehaviour
+{
+    int DispensingFrames = 20;
+    int counter = 0;
+    public GameObject dispensable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v;
+        counter++;
+        if (counter == DispensingFrames) {
+            GameObject x = Instantiate(dispensable);
+            ProjectileScript y = x.GetComponent<ProjectileScript>();
+            v = new Vector2(Random.Range(-1.0f, 1 - 0f), Random.Range(-1.0f, 1 - 0f));
+            y.direction = v.normalized;
+            y.speed = 0.2f;
+            counter++;
+            counter %= 4;
+        }
+    }
+}
---
+++ GrupaTNT/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public Vector2 direction;
+    public float speed=0.0f;
+    int TTL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TTL = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(direction);
+        transform.Translate(direction * speed);
+        TTL--;
+        if (TTL == 0 && speed!=0.0f) { GameObject.Destroy(gameObject); }
+    }
+}
