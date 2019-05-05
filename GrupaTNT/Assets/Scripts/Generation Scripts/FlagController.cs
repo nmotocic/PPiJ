b14713cb9117aee7de91f49e3382367c90ff0b0e
Name <matija.zlovolic@fer.hr>
2019-05-05 02:14:13 +0200

    Created Flag Controller

---
+++ GrupaTNT/Assets/Scripts/Generation Scripts/FlagController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    /// <summary>
+    /// We manually set the sprites in the inspector
+    /// </summary>
+    [System.Serializable]
+    public class FlagTypes
+    {
+        public Sprite PowerUpSpawn;
+        public Sprite EnemySpawn;
+        public Sprite DoorRight;
+        public Sprite DoorLeft;
+        public Sprite DoorUp;
+        public Sprite DoorDown;
+    }
+
+    public FlagTypes flagTypes;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
