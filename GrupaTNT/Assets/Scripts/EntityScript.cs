d15a1babeb94d31314e1090c46895b25b5362a46
Dorijan_Cirkveni <dorijan.cirkveni@engineer.com>
2019-05-08 14:37:21 +0200

    (Implementacija powerupa)
    Dodana klasa FloatStat

--- GrupaTNT/Assets/Scripts/EntityScript.cs
+++ GrupaTNT/Assets/Scripts/EntityScript.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class EntityScript : MonoBehaviour
 {
     public EntityControllerInterface controller;
+    public Dictionary<string, string> stats; //Za sada public
     public List<GameObject> projectileOptions = new List<GameObject>();
     List<GameObject> firedProjectiles = new List<GameObject>();
     Rigidbody2D rb2d;
     public float speed=5;
-    public string entityType= "player";
+    public string entityType;
     // Start is called before the first frame update
     public void Init(string entityType,Vector2 location,Vector2 direction,float speed)
     {
         gameObject.transform.position = location;
         gameObject.SetActive(true);
         this.entityType = entityType;
-        controller = this.gameObject.GetComponent<EntityControllerInterface>();
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         if (rb2d == null) { rb2d = gameObject.AddComponent<Rigidbody2D>(); }
         if (entityType.Equals("player")) { controller = new PlayerController(this,speed); }
-        else if (!entityType.Equals("projectiles")) { controller = new ProjectileController(this,direction,speed); }
+        else if (entityType.Equals("projectile")) { controller = new ProjectileController(this,direction,speed); }
+        else if (entityType.Equals("powerup")) { controller = new ProjectileController(this, direction, speed); }
     }
     public void Start()
     {
@@ -47,6 +48,7 @@ public class EntityScript : MonoBehaviour
     {
         GameObject x = Instantiate(dispensable);
         EntityScript y = x.AddComponent<EntityScript>();
+        Debug.Log(direction.ToString() + " " + speed.ToString());
         y.Init("projectile",location,direction,speed);
     }
     Vector2 GetLocation() {
--- GrupaTNT/Assets/Scripts/PlayerController.cs
+++ GrupaTNT/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : EntityControllerInterface
 {
     EntityScript parentScript;
     Vector2 direction;
+    Vector2 lastDirection;
     float speed = 2f;
     float health = 20f;
     // Start is called before the first frame update
@@ -19,10 +20,11 @@ public class PlayerController : EntityControllerInterface
         float X = Input.GetAxis("Horizontal");
         float Y = Input.GetAxis("Vertical");
         direction = new Vector2(X, Y);
+        lastDirection = (direction == new Vector2(0f, 0f)) ? lastDirection : direction;
         if (Input.GetMouseButtonDown(0)||Input.GetKeyDown("q"))
         {
             Vector2 position = parentScript.gameObject.transform.position;
-            parentScript.DispenseObject(parentScript.projectileOptions[0], position+direction*0.01f, direction.normalized,20f);
+            parentScript.DispenseObject(parentScript.projectileOptions[0], position+lastDirection*0.01f, lastDirection.normalized,20f);
         }
     }
     public Vector2 getMovement() { return direction*speed; }
