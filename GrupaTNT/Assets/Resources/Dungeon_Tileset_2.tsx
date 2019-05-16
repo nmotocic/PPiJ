cffd6372a4bd25af143ec6a8bec9afa8637d18f9
Name <matija.zlovolic@fer.hr>
2019-05-16 10:50:59 +0200

    Change to use Tiled+SuperTiled2Unity

---
+++ GrupaTNT/Assets/Resources/Dungeon_Tileset_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Dungeon_Tileset_2" tilewidth="32" tileheight="32" tilecount="320" columns="16">
+ <image source="dungeon_tileset_2.png" width="512" height="640"/>
+</tileset>
