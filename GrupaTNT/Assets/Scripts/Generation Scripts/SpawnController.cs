74c6e9dbc3500fd9cfab00bd8f62797b48d43ba2
Name <matija.zlovolic@fer.hr>
2019-05-25 17:56:00 +0200

    Spawning system start

---
+++ GrupaTNT/Assets/Scripts/Generation Scripts/SpawnController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : Singleton<SpawnController>
+{
+    [SerializeField] public GameObject[] Enemys;
+    [SerializeField] public GameObject[] PowerUps;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
