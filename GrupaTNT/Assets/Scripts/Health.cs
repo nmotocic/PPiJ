29e843c12d6fe6a691b9ae54e1f0f383dc0d4ef4
nmotocic <nikolina.motocic1@gmail.com>
2019-06-04 20:28:37 +0200

    hud, skills

---
+++ GrupaTNT/Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] lives;
+    public Sprite heartFull;
+    public Sprite heartEmpty;
+
+    private void Update()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < lives.Length; i++)
+        {
+
+
+            if (i < health)
+            {
+                lives[i].sprite = heartFull;
+            }
+            else
+            {
+                lives[i].sprite = heartEmpty;
+            }
+            if (numOfHearts > i)
+            {
+                lives[i].enabled = true;
+            }
+            else
+            {
+                lives[i].enabled = false;
+            }
+        }
+    }
+}
