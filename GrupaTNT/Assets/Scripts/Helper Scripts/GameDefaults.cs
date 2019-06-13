fcfcec0baedbc3ca9e38c2b899d4db408b4f27b9
Tomislav <tomislav.domlija@fer.hr>
2019-06-13 20:20:07 +0200

    Dodana mogucnost multishota. Dodan novi projektil za igraca.

--- GrupaTNT/Assets/Scripts/Helper Scripts/GameDefaults.cs
+++ GrupaTNT/Assets/Scripts/Helper Scripts/GameDefaults.cs
@@ -49,4 +49,32 @@ public abstract class GameDefaults
     public static int corpseTimeout() {
         return 10;
     }
+
+
+    //Helper scripts
+    /// <summary>
+    /// Turns a normalised V2 into degrees.
+    /// </summary>
+    /// <param name="vect">Normalised vector2</param>
+    /// <returns>Angle</returns>
+    public static float vectToAngle(Vector2 vect) {
+        float angle = Vector2.Angle(vect, Vector2.right);
+        if (vect.y < 0) angle *= -1;
+        return angle;
+    }
+
+    public static Vector2 angleToVect(float angle) {
+        angle *= Mathf.Deg2Rad;
+        Vector2 vect = new Vector2(0,0);
+        vect.y=Mathf.Sin(angle);
+        vect.x = Mathf.Cos(angle);
+        return vect;
+    }
+
+    public static Vector2 rotateVector(Vector2 vect, float angle) {
+        float rads = angle * Mathf.Deg2Rad;
+        float x = vect.x * Mathf.Cos(rads) - vect.y * Mathf.Sin(rads);
+        float y = vect.x * Mathf.Sin(rads) - vect.y * Mathf.Cos(rads);
+        return new Vector2(x, y);
+    }
 }
--- GrupaTNT/Assets/Scripts/PlayerController.cs
+++ GrupaTNT/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,27 @@ public class PlayerController : EntityControllerInterface
     PlayerSpriteRenderer spriteAnimator;
     Vector2 direction;
     float speed = 2f;
-    public float health = 100f;
+    public float health = 10f;
     Alarm attackDelay = new Alarm(0);
     // Start is called before the first frame update
     public PlayerController(EntityScript ps,float speed=0f) {
         parentScript = ps;
         spriteAnimator = parentScript.gameObject.GetComponent<PlayerSpriteRenderer>();
         if (speed == 0f) { speed = this.speed; }
-        parentScript.stats.Add("health", new FloatStat("health", health));
-        parentScript.stats.Add("ranged", new FloatStat("ranged", 1));
-        parentScript.stats.Add("damage", new FloatStat("ranged", 1));
-        parentScript.stats.Add("attackSpeed", new FloatStat("attackSpeed", 0.5f)); // Delay between attacks
-        parentScript.stats.Add("gold", new FloatStat("gold", 0.01f));
-        parentScript.stats.Add("experience", new FloatStat("experience", 0));
-        parentScript.stats.Add("armor", new FloatStat("armor", 5));
-        parentScript.stats.Add("speed", new FloatStat("speed", speed));
+        if (!parentScript.valsInit)
+        {
+            parentScript.stats.Add("health", new FloatStat("health", health));
+            parentScript.stats.Add("ranged", new FloatStat("ranged", 1));
+            parentScript.stats.Add("damage", new FloatStat("ranged", 1));
+            parentScript.stats.Add("attackSpeed", new FloatStat("attackSpeed", 0.5f)); // Delay between attacks
+            parentScript.stats.Add("gold", new FloatStat("gold", 0.01f));
+            parentScript.stats.Add("experience", new FloatStat("experience", 0));
+            parentScript.stats.Add("armor", new FloatStat("armor", 5));
+            parentScript.stats.Add("speed", new FloatStat("speed", speed));
+            parentScript.stats.Add("rangedCount", new FloatStat("rangedCount", 1)); //Broj projektila
+            parentScript.stats.Add("rangedSpread", new FloatStat("rangedSpread", 15)); //Kut pucanja
+            parentScript.valsInit = true;
+        }
     }
     
     // Update is called once per frame
@@ -43,10 +49,31 @@ public class PlayerController : EntityControllerInterface
         {
             Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 position = parentScript.gameObject.transform.position;
+            Vector2 shootVector = (target - position).normalized;
             string[] rawInput= {"EFFECT damage boop 1 -1 1"};
-            parentScript.DispenseObject(parentScript.projectileOptions[0], position, (target-position).normalized,20f,rawInput);
-            attackDelay.reset();
+            float angle = 0,diff=0;
+            int cnter = Mathf.RoundToInt(parentScript.stats["rangedCount"].getCompoundValue());
+            if (cnter <= 1)
+            { //Barem jedan projektil
+                cnter = 1;
+            }
+            else { //Izracunaj kuteve
+                diff = Mathf.Abs(parentScript.stats["rangedSpread"].getCompoundValue() / (parentScript.stats["rangedCount"].getCompoundValue()-1));
+                angle = GameDefaults.vectToAngle(shootVector);
+                angle -= Mathf.Abs(parentScript.stats["rangedSpread"].getCompoundValue()/2);
+                shootVector = GameDefaults.angleToVect(angle);
+                //Debug.LogWarning("Angle:" + angle.ToString());
+            }
+            for (int i = 0; i < cnter;i++){
+                parentScript.DispenseObject(parentScript.projectileOptions[0], position, shootVector, 20f, rawInput);
+                angle += diff;
+                //if (cnter % 2 == 0 && cnter==2) angle += diff;
+                shootVector = GameDefaults.angleToVect(angle);
+                //Debug.LogWarning("ShootVector:" + target.ToString() + "-" + position.ToString() + "=" + shootVector.ToString());
+
+            }
             if(spriteAnimator!=null)spriteAnimator.attack = true;
+            attackDelay.reset();
         }
     }
     public Vector2 getMovement() { return direction*parentScript.stats["speed"].getCompoundValue(); }
